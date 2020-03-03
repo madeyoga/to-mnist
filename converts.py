cca4fcc73e6e74a19c3b9d634561b779f08fdb3c
MadeYoga <vngla21@gmail.com>
2020-03-03 23:44:20 +0700

    First commit.

---
+++ converts.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import numpy as np
+
+path = "numbers/"
+
+list_of_files = os.listdir(path)
+
+for i, file in enumerate(list_of_files):
+    image_file_name = os.path.join(path, file)
+    if ".png" in image_file_name:
+        img = cv2.imread(image_file_name, cv2.IMREAD_GRAYSCALE)
+        dimension = (14, 14)
+        img = cv2.resize(img, dimension, 0, 0, interpolation=cv2.INTER_AREA)
+        img = img[0:12]
+        img = cv2.resize(img, dimension, 0, 0, interpolation=cv2.INTER_AREA)
+        (thresh, img) = cv2.threshold(img,
+                                      128,
+                                      255,
+                                      cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        img = 255-img
+        file = "resized/number" + str(i) + ".png"
+        blank_image = np.zeros(shape=[28, 28], dtype=np.uint8)
+        blank_image[7:21, 7:21] = img
+        blank_image = blank_image.reshape(28, 28)
+        cv2.imwrite(file, blank_image)
