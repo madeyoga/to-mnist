7be00eaf1b790cdc068ac9c75355c28e91c7646d
MadeYoga <vngla21@gmail.com>
2020-03-21 18:45:31 +0700

    Update converts with aspect ratio method. aspect ratio & normalize center.

--- converts.py
+++ converts.py
@@ -1,25 +1,28 @@
 import os
 import cv2
 import numpy as np
+from natsort import natsorted
 
 path = 'G:\\Kuliah\\skripsi\\Project\\RLSA\\segmented_number\\' # "numbers/"
 
 list_of_files = os.listdir(path)
+list_of_files = natsorted(list_of_files)
 
 for i, file in enumerate(list_of_files):
-    image_file_name = os.path.join(path, file)
+    image_file_name = path+file
+    print(image_file_name)
     if ".png" in image_file_name:
         img = cv2.imread(image_file_name, cv2.IMREAD_GRAYSCALE)
         dimension = (16, 16)
         img = cv2.resize(img, dimension, 0, 0, interpolation=cv2.INTER_AREA)
-        img = img[0:13]
-        img = cv2.resize(img, dimension, 0, 0, interpolation=cv2.INTER_AREA)
+##        img = img[0:13]
+##        img = cv2.resize(img, dimension, 0, 0, interpolation=cv2.INTER_AREA)
         (thresh, img) = cv2.threshold(img,
                                       128,
                                       255,
                                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)
         img = 255-img
-        file = "resized/number" + str(i) + ".png"
+        file = "resized/mnist" + str(i) + ".png"
         blank_image = np.zeros(shape=[28, 28], dtype=np.uint8)
         blank_image[7:23, 7:23] = img
         blank_image = blank_image.reshape(28, 28)
--- converts_with_aspect_ratio.py
+++ converts_with_aspect_ratio.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+from natsort import natsorted
 
 ##Image:
 ##current_width 
@@ -12,35 +13,44 @@ import numpy as np
 ##
 ##size = (new_width, 113)
 
-path = "numbers/"
+path = 'G:\\Kuliah\\skripsi\\Project\\RLSA\\segmented_number\\'
 
 list_of_files = os.listdir(path)
+list_of_files = natsorted(list_of_files)
+
+mnist_x = 28
+mnist_y = 28
+center_x, center_y = int(mnist_x/2), int(mnist_y/2)
 
 for i, file in enumerate(list_of_files):
-    image_file_name = os.path.join(path, file)
+    image_file_name = path + file
     if ".png" in image_file_name:
         img = cv2.imread(image_file_name, cv2.IMREAD_GRAYSCALE)
-        cv2.imshow("original", img)
         
         # Aspect Ratio
         current_height = img.shape[0]
         current_width = img.shape[1]
         
-        new_height = 14
+        new_height = 20
         new_width = int(current_width * (new_height / current_height))
 
         dimension = (new_width, new_height)
         
         img = cv2.resize(img, dimension, 0, 0, interpolation=cv2.INTER_AREA)
-        cv2.imshow("resize", img)
         
         (thresh, img) = cv2.threshold(img,
                                       128,
                                       255,
                                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)
         img = 255-img
-        file = "resized2/number" + str(i) + ".png"
+        file = "resize_with_aspect_ratio/number" + str(i) + ".png"
         blank_image = np.zeros(shape=[28, 28], dtype=np.uint8)
-        blank_image[7:21, 7:7+dimension[0]] = img
+
+        # Calculate offset
+        start_x = center_x - int(0.5 * new_width)
+        start_y = center_y - int(0.5 * new_height)
+
+        # Place center
+        blank_image[start_y:start_y+new_height, start_x:start_x+new_width] = img
         blank_image = blank_image.reshape(28, 28)
         cv2.imwrite(file, blank_image)
